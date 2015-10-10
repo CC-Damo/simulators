60a931ba7875abb268b99140f0a04b1eeb92efaa
BuildTools <unconfigured@null.spigotmc.org>
2015-10-10 15:08:36 +0100

    GitHub Pages Push

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function HideContent(d) {
+document.getElementById(d).style.display = "none";
+}
+
+function ShowContent(d) {
+document.getElementById(d).style.display = "block";
+}
